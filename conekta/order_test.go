1fcbeb9c631e6be06de5f2323548e92414a51c3c
Sergio Contreras Castañeda <checo1085@hotmail.com>
2018-10-15 15:07:17 -0700

    Add discount test

--- conekta/order_test.go
+++ conekta/order_test.go
@@ -119,4 +119,37 @@ var _ = Describe("Handle order", func() {
 			Expect(statusCode).Should(Equal(200))
 		})
 	})
+	Context("Create a order With Discount Line", func() {
+		It("Should response 200", func() {
+			//New Order
+			order := new(conekta.Order)
+			item := conekta.LineItem{
+				Name:        "Burritos",
+				Description: "Made in Mexico.",
+				UnitPrice:   45000,
+				Quantity:    2,
+			}
+			order.LineItems = append(order.LineItems, item)
+			order.Currency = "MXN"
+			charge := conekta.Charge{
+				PaymentMethod: conekta.PaymentMethod{
+					Type:    "card",
+					TokenId: "tok_test_visa_4242",
+				},
+			}
+			order.Charges = append(order.Charges, charge)
+			discount := conekta.DiscountLine{
+				Code:   "BURO1",  // Your custom code
+				Type:   "coupon", // It can be loyalty, campaign, coupon or sign. https://developers.conekta.com/api?language=bash#discount-line
+				Amount: 1000,
+			}
+			order.DiscountLines = append(order.DiscountLines, discount)
+			order.CustomerInfo.Name = "Fulanito Pérez"
+			order.CustomerInfo.Email = "fulanito@conekta.com"
+			order.CustomerInfo.Phone = "+52181818181"
+			statusCode, _, response := order.Create()
+			oid = response.ID
+			Expect(statusCode).Should(Equal(200))
+		})
+	})
 })
